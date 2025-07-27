7b3548715206f53596b1150c10642508b54be2c7
Muhammed Mehti GÜLER <mehti.guler@gmail.com>
2025-07-27 17:29:10 +0300

    Add project files.

---
+++ src/GulerRentServer.Application/RegistrarService.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using GulerRentServer.Application.Behaviors;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GulerRentServer.Application
+{
+    public static class RegistrarService
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(cfr =>
+            {
+                cfr.RegisterServicesFromAssembly(typeof(RegistrarService).Assembly);
+                cfr.AddOpenBehavior(typeof(ValidationBehavior<,>));
+                cfr.AddOpenBehavior(typeof(PermissionBehavior<,>));
+            });
+            services.AddValidatorsFromAssembly(typeof(RegistrarService).Assembly);
+           
+            return services;
+        }
+    }
+}
